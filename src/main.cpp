954572c436bf799aff87274427f5a532e7df4e98
uz. <ukghori08@gmail.com>
2023-12-21 16:17:43 -0500

    renaming & file changes

--- src/main.cpp
+++ src/main.cpp
@@ -57,16 +57,16 @@ void autonomous()
 	//move for seconds
 	leftmotor_group.move(127);
 	rightmotor_group.move(-127);
-	pros::delay(550);
+	pros::delay(675);
 	leftmotor_group.move(0); 
     rightmotor_group.move(0); 
 
 	//rotate and extend wings
 	wingflaps.move(-127);
-	pros::delay(1500);
+	pros::delay(1200);
 	wingflaps.move(0);
-	leftmotor_group.move(127);
-	rightmotor_group.move(127);
+	leftmotor_group.move(-127);
+	rightmotor_group.move(-127);
 	pros::delay(420);
 	leftmotor_group.move(0); 
     rightmotor_group.move(0); 
@@ -125,20 +125,6 @@ void autonomous()
     rightmotor_3.move_velocity(0);
     wingflaps.move_velocity(0);
 }
-
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 void opcontrol()
 {
 	// pros::Controller master(pros::E_CONTROLLER_MASTER);
